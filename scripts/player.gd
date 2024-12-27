25959f4db6c20d8b0e62acfb98f3b78994c6f725
Konstantin <119509915+KonstantinKolo@users.noreply.github.com>
2024-12-28 00:12:54 +0200

    Added the character,  locomotive + fighting animations, and sprinting functionality

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,24 @@
 extends CharacterBody3D
 
+@onready var camera_mount: Node3D = $camera_mount
+@onready var animation_player: AnimationPlayer = $visuals/MainCharacter/AnimationPlayer
 
-const SPEED = 5.0
+var speed = 2
 const JUMP_VELOCITY = 4.5
 
-@onready var pivot: Node3D = $CameraOrigin
-@export var sens = 0.5
+var reverseAnimBool = false;
+
+@export var sens_horizontal = 0.5
+@export var sens_vertical = 0.5
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	animation_player.animation_finished.connect(self._on_animation_player_animation_finished)
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * sens))
-		pivot.rotate_x(deg_to_rad(-event.relative.y * sens))
-		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+		rotate_y(deg_to_rad(-event.relative.x*sens_horizontal))
+		camera_mount.rotate_x(deg_to_rad((-event.relative.y*sens_vertical)))
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -30,10 +34,59 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		if animation_player.current_animation != "a-walk" && animation_player.current_animation != "a-idle-to-walk" && animation_player.current_animation != "a-walk-to-run" && !Input.is_action_pressed("sprint"):
+			if animation_player.current_animation == "a-run":
+				animation_player.play_backwards("a-walk-to-run")
+				reverseAnimBool = true
+			else:
+				animation_player.play("a-idle-to-walk")
+				speed = 2
+		elif animation_player.current_animation != "a-run" && animation_player.current_animation != "a-idle-to-run" && animation_player.current_animation != "a-walk-to-run" && Input.is_action_pressed("sprint"):
+			if animation_player.current_animation == "a-walk":
+				animation_player.play("a-walk-to-run")
+			else:
+				animation_player.play("a-idle-to-run")
+				speed = 4
+		
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		if animation_player.current_animation != "a-idle":
+			_reverse_animation_to_idle()
+			
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "a-idle-to-walk" && !reverseAnimBool:
+		animation_player.play("a-walk")
+	elif anim_name == "a-idle-to-walk" && reverseAnimBool:
+		animation_player.play("a-idle")
+		reverseAnimBool = false
+	elif anim_name == "a-idle-to-run" && !reverseAnimBool:
+		animation_player.play("a-run")
+	elif anim_name == "a-idle-to-run" && reverseAnimBool:
+		animation_player.play("a-idle")
+		reverseAnimBool = false
+	elif anim_name == "a-walk-to-run" && reverseAnimBool:
+		animation_player.play("a-walk")
+		speed = 2
+		reverseAnimBool = false;
+	elif anim_name == "a-walk-to-run" && !reverseAnimBool:
+		animation_player.play("a-run")
+		speed = 4
+	else:
+		print("finished animation:", anim_name)
+
+func _reverse_animation_to_idle() -> void:
+	pass
+	if animation_player.current_animation == "a-walk":
+		animation_player.play_backwards("a-idle-to-walk")
+		reverseAnimBool = true
+	elif animation_player.current_animation == "a-run":
+		animation_player.play_backwards("a-idle-to-run")
+		reverseAnimBool = true
+		speed = 2
